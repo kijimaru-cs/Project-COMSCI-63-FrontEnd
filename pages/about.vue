73d01ed9be4716043abc121b1cdb284ea74efe08
kijimaru-cs <suravut2539@hotmail.com>
2020-07-09 15:07:27 +0700

    update 9/7/63

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="con-center">
+   <h1>About</h1>
+   <h2>อันนี้คือหน้า page about</h2> 
+  </div>
+</template>
+<style>
+  .con-center {
+     text-align: center;
+     margin-top: 1rem;
+  }
+</style>
---
+++ pages/vue-ability/01-data-binding.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="con-center">
+    <h1>Hello {{ name }}</h1>
+    <input type="text" :value="name"/>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+      setInterval(()=>{
+          this.name += "*"
+      },3000)
+    return {
+      name: "Artdvp"
+    }
+  }
+}
+</script>
+<style>
+.con-center {
+  text-align: center;
+  margin-top: 1rem;
+  background-color: white;
+  color: black;
+}
+</style>
